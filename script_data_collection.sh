7ae617e71919a966fa7fd7516393efa20b3a9b9d
HZjiadong <guojiadonghz@gmail.com>
2022-03-22 16:08:12 +0100

    feat: change for script

--- script_data_collection.sh
+++ script_data_collection.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-N="4096 4096 1"
+N="4096 8192 16384"
 BS="64 128 256 512 1024 2048 4096"
 
 for n in $N
@@ -9,14 +9,16 @@ do
   for bs in $BS
   do
     echo "----- run GEMM $n x $n, BS=$bs"
-    ./executable $n $bs
+    g++ -g main.cpp -o executable -I${CUDA_HOME}/include -L${CUDA_HOME}/lib64 -lcublas -lcurand -fopenmp -lcudart
+    numactl -m 0 -C 0 ./executable 10240
 
     #Move .csv file to directory $dirname
     prefix=`date +"%Y-%m-%d"`
     dir=$prefix/$n/$bs
     mkdir -p $dir
-    mv cpatureTime.csv $dir
+    mv captureTime.csv $dir
     mv instantiationTime.csv $dir
     mv launchingTime.csv $dir
+    rm -f executable
   done
 done
