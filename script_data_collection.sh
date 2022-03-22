625562e4f41ce454fb5f240c59b25cb9419ff877
HZjiadong <guojiadonghz@gmail.com>
2022-03-22 16:16:19 +0100

    fix: script debug 1 2022-03-22

--- script_data_collection.sh
+++ script_data_collection.sh
@@ -3,14 +3,13 @@
 
 N="4096 8192 16384"
 BS="64 128 256 512 1024 2048 4096"
-
+make executable
 for n in $N
 do
   for bs in $BS
   do
     echo "----- run GEMM $n x $n, BS=$bs"
-    g++ -g main.cpp -o executable -I${CUDA_HOME}/include -L${CUDA_HOME}/lib64 -lcublas -lcurand -fopenmp -lcudart
-    numactl -m 0 -C 0 ./executable 10240
+    numactl -m 0 -C 0 ./executable $n $bs
 
     #Move .csv file to directory $dirname
     prefix=`date +"%Y-%m-%d"`
