6be8d62df18962bcba0170d564d0bd6598956408
HZjiadong <guojiadonghz@gmail.com>
2022-03-31 18:18:51 +0200

    feat: add log file

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+log=log_file.txt
+printf "This is the Log File of date" > $log
 
 N="7680 15360 23040 30720 38400"
 BS="64 128 192 256 320 384 512 640 768 960 1280 1536 1920 2560 3840 7680"
@@ -8,8 +10,8 @@ for n in $N
 do
   for bs in $BS
   do
-    echo "----- run GEMM $n x $n, BS=$bs"
-    numactl -m 0 -C 0 ./executable $n $bs
+    echo "----- run GEMM $n x $n, BS=$bs" >> $log    
+    numactl -m 0 -C 0 ./executable $n $bs >> $log
 
     #Move .csv file to directory $dirname
     prefix=`date +"%Y-%m-%d"`
@@ -23,10 +25,13 @@ done
 
 for n in $N
 do
-  cat $prefix/$n/*/*.csv > ${n}.csv
-  sort -u -n ${n}.csv > ${n}_all.csv
-  ./analysis.r ${n}_all.csv
+  cat $prefix/$n/*/*.csv > ${n}.csv >> $log
+  sort -u -n ${n}.csv > ${n}_all.csv >> $log
+  ./analysis.r ${n}_all.csv >> $log
 done
+
+dir2 = $prefix
+mv log_file.txt $dir2
 
 rm -f captureTime.csv
 rm -f instantiationTime.csv
