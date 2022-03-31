382a3ff355170bcad9c12ca90ebb429e7e50ce07
HZjiadong <guojiadonghz@gmail.com>
2022-03-31 21:02:43 +0200

    fix: several debugs for script

--- script.sh
+++ script.sh
@@ -2,13 +2,11 @@
 
 log=log_file.txt
 printf "This is the Log File of date" > $log
-
+timer_start=`date +"%Y-%m-%d %H:%M:%S"`
+echo "start Time "$timer_start >> $log
 N="7680 15360 23040 30720 38400"
 BS="64 128 192 256 320 384 512 640 768 960 1280 1536 1920 2560 3840 7680"
 make executable
-
-timer_start = date "+%Y-%m-%d %H-%M-%S" >> $log
-
 for n in $N
 do
   for bs in $BS
@@ -33,11 +31,10 @@ do
   ./analysis.r ${n}_all.csv >> $log
 done
 
-timer_end = date "+%Y-%m-%d %H-%M-%S" >> $log
-duration = `echo eval $(($(date +%s -d "${timer_end}") - $(date +%s -d "${timer_start}"))) | awk '{t=split("60 s 60 m 24 h 999 d",a);for(n=1;n<t;n+=2){if($1==0)break;s=$1%a[n]a[n+1]s;$1=int($1/a[n])}print s}'`
-echo "This experience has took： $duration" >> $log
+timer_end=`date +"%Y-%m-%d %H:%M:%S"`
+echo "end Time "$timer_end >> $log
 
-dir2 = $prefix
+dir2=$prefix
 mv log_file.txt $dir2
 
 rm -f captureTime.csv
