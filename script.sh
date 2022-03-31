b1505322c0010f748a0315d49362fe56845a7911
HZjiadong <guojiadonghz@gmail.com>
2022-03-31 22:15:03 +0200

    fix: try 22:30 2022-03-31

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-
+log=log_file.txt
+printf "This is the Log File of date" > $log
+timer_start=`date +"%Y-%m-%d %H:%M:%S"`
+echo "start Time "$timer_start >> $log
 N="1280 2560 3840 5120 6400 7680 8960 10240 11520 12800 14080 15360 16640 17920 19200 20480 21760 23040 24320 25600 26880 28160 29440 30720"
 BS="64 128 256 320 640 1280"
 make executable
@@ -8,8 +11,8 @@ for n in $N
 do
   for bs in $BS
   do
-    echo "----- run GEMM $n x $n, BS=$bs"
-    numactl -m 0 -C 0 ./executable $n $bs
+    echo "----- run GEMM $n x $n, BS=$bs" >> $log    
+    numactl -m 0 -C 0 ./executable $n $bs >> $log
 
     #Move .csv file to directory $dirname
     prefix=`date +"%Y-%m-%d"`
@@ -23,10 +26,16 @@ done
 
 for n in $N
 do
-  cat $prefix/$n/*/*.csv > ${n}.csv
-  sort -u -n ${n}.csv > ${n}_all.csv
-  ./analysis.r ${n}_all.csv
+  cat $prefix/$n/*/*.csv > ${n}.csv >> $log
+  sort -u -n ${n}.csv > ${n}_all.csv >> $log
+  ./analysis.r ${n}_all.csv >> $log
 done
+
+timer_end=`date +"%Y-%m-%d %H:%M:%S"`
+echo "end Time "$timer_end >> $log
+
+dir2=$prefix
+mv log_file.txt $dir2
 
 rm -f captureTime.csv
 rm -f instantiationTime.csv
