e70089a3e6a354a109e10d342b57ddb466361f98
HZjiadong <guojiadonghz@gmail.com>
2022-04-11 14:53:34 +0200

    debug 'operation' term and color print error

--- yesCudaGraph/analysis.r
+++ yesCudaGraph/analysis.r
@@ -13,7 +13,7 @@ readfile <- function (filename)
 
 #Merge several measures of the same point together
 # Return a data frame with columns :
-# - mean = the mean of time values for each group with same (dimension,blocksize,kernel)
+# - mean = the mean of time values for each group with same (dimension,blocksize,kernel,operation)
 # - sd : the standard devision of the time values (of each group)
 # - n : the number of data values in each group
 # - min_time/max_time: the min/max of the time values of each group.
@@ -23,7 +23,7 @@ makestats<- function(dfraw)
 	citol= 0.95
 	df <- dfraw %>% filter(index != 0) %>%
 	      # make group of data with same value for following column:
-	      group_by(dimension,blocksize,kernel) %>% 
+	      group_by(dimension,blocksize,kernel,operation) %>% 
 	      # summarize goup of data by computing time= mean of time of each group, etc
 	      summarize( 
 	          n=length(time),
@@ -71,11 +71,13 @@ theplot1 = ggplot() +
        legend.title =  element_text("Helvetica")
    ) +
    guides(fill = guide_legend(nrow = 1)) +
-   geom_line(data=l, aes(x=kernel,
-                         y=mean
+   geom_line(data=l, aes(color=operation,
+                        x=kernel,
+                        y=mean
                         )) +
-   geom_point(data=l, aes(x=kernel,
-                         y=mean
+   geom_point(data=l, aes(color=operation,
+                        x=kernel,
+                        y=mean
                         )) +
    facet_grid( ~dimension);
 
@@ -96,10 +98,12 @@ theplot2 = ggplot() +
        legend.title =  element_text("Helvetica")
    ) +
    guides(fill = guide_legend(nrow = 1)) +
-   geom_line(data=l, aes(x=blocksize,
+   geom_line(data=l, aes(color=operation,
+                         x=blocksize,
                          y=mean
                         )) +
-   geom_point(data=l, aes(x=blocksize,
+   geom_point(data=l, aes(color=operation,
+                         x=blocksize,
                          y=mean
                         )) +
    facet_grid( ~dimension);
@@ -121,14 +125,15 @@ theplot3 = ggplot() +
        legend.title =  element_text("Helvetica")
    ) +
    guides(fill = guide_legend(nrow = 1)) +
-   geom_point(data=df, aes(x=blocksize,
+   geom_point(data=df, aes(color=operation,
+                         x=blocksize,
                          y=time
                         )) +
    facet_grid( ~dimension);
 
 # theplot4: line chart
 # - X: number of kernels
-# - Y: total tera-flops 
+# - Y: total tera-flops for each operation
 theplot4 = ggplot() +
   theme_bw(base_size=16) +
    xlab("#kernels") +
@@ -155,7 +160,7 @@ theplot4 = ggplot() +
 
 # theplot5: scatter plot
 # - X: number of kernels
-# - Y: total tera-flops 
+# - Y: total tera-flops for each operation
 theplot5 = ggplot() +
   theme_bw(base_size=16) +
    xlab("#blocksize") +
