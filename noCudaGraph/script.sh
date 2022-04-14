a9c91d6d54bacd5637d49d31f73dec841bd9beec
HZjiadong <guojiadonghz@gmail.com>
2022-04-14 11:58:47 +0200

    fix bugs of script

--- noCudaGraph/script.sh
+++ noCudaGraph/script.sh
@@ -4,8 +4,9 @@ log=log_file_noCudaGraph.txt
 printf "This is the Log File of date" > $log
 timer_start=`date +"%Y-%m-%d %H:%M:%S"`
 echo "start Time "$timer_start >> $log
+prefix=`date +"%Y-%m-%d"`
 N="1280 2560 3840 5120 6400 7680 8960 10240 11520 12800 14080 15360 16640 17920 19200 20480 21760 23040 24320 25600 26880 28160 29440 30720 32000 33280 34560 35840 37120 38400"
-BS="64 128 256 320 640 1280"
+BS="64 128 256 320 640 1280 2560 3840 5120 6400 7680 8960 10240 11520 12800 14080 15360 16640 17920 19200 20480 21760 23040 24320 25600 26880 28160 29440 30720 32000 33280 34560 35840 37120 38400"
 make executable
 for n in $N
 do
@@ -15,7 +16,6 @@ do
     numactl -m 0 -C 0 ./executable $n $bs >> $log
 
     #Move .csv file to directory $dirname
-    prefix=`date +"%Y-%m-%d"`
     dir=$prefix/$n/$bs
     mkdir -p $dir
     mv executionTime.csv $dir
