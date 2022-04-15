5fc16ec39654c53c1bb9aef6b4de5589212a4495
HZjiadong <guojiadonghz@gmail.com>
2022-04-15 19:23:27 +0200

    modification of script for same time GPU different experiences test

--- yesCudaGraph/script.sh
+++ yesCudaGraph/script.sh
@@ -5,15 +5,15 @@ printf "This is the Log File of date" > $log
 timer_start=`date +"%Y-%m-%d %H:%M:%S"`
 echo "start Time "$timer_start >> $log
 prefix=`date +"%Y-%m-%d"`
-N="1280 2560 3840 5120 6400 7680 8960 10240 11520 12800 14080 15360 16640 17920 19200 20480 21760 23040 24320 25600 26880 28160 29440 30720 32000 33280 34560 35840 37120 38400"
-BS="64 128 256 320 640 1280 2560 3840 5120 6400 7680 8960 10240 11520 12800 14080 15360 16640 17920 19200 20480 21760 23040 24320 25600 26880 28160 29440 30720 32000 33280 34560 35840 37120 38400"
+N="1280 5120 8960 12800 16640 20480 24320 28160 32000 35840"
+BS="64 128 256 320 640 1280 5120 8960 12800 16640 20480 24320 28160 32000 35840"
 make executable
 for n in $N
 do
   for bs in $BS
   do
-    echo "----- run GEMM $n x $n, BS=$bs" >> $log    
-    numactl -m 0 -C 0 ./executable $n $bs >> $log
+    echo "----- run GEMM $n x $n, BS=$bs" >> $log  
+    CUDA_VISIBLE_DEVICES=1 numactl -m 0 -C 0 ./executable $n $bs >& $log &
 
     #Move .csv file to directory $dirname
     dir=$prefix/$n/$bs
