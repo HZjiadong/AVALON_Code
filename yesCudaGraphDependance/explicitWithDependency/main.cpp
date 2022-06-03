31c2a092c517d7ba03ea45e62d9ef7177b32c5f2
HZjiadong <guojiadonghz@gmail.com>
2022-06-03 13:36:36 +0200

    add loop inside main function

--- yesCudaGraphDependance/explicitWithDependency/main.cpp
+++ yesCudaGraphDependance/explicitWithDependency/main.cpp
@@ -153,6 +153,8 @@ int main(int argc, char *argv[]) {
     //Output data
     cudaGraph_t graph;
     checkCudaErrors(cudaGraphCreate(&graph, 0));
+
+for (int k=0; k < 200; k ++){
     clock_gettime(CLOCK_REALTIME, &start_time);
     call_kernel_number = gpu_blas_mmul(d_A, d_B, d_C, nr_rows_A, nr_cols_A, nr_cols_B, graph);
     clock_gettime(CLOCK_REALTIME, &end_time);
@@ -160,7 +162,9 @@ int main(int argc, char *argv[]) {
     operation_type = "graphApiYesDependency";
     cudagraph = 1;
     cudaGraphDependencyCsv << dependencyApiTime << "," << call_kernel_number << "," << dimension << "," << BS << "," << operation_type << "," << cudagraph << endl;
-    printf("Elapsed time for graph capture:%f (s)\n", time_to_double(time_diff(start_time, end_time)));    
+    printf("Elapsed time for graph capture:%f (s)\n", time_to_double(time_diff(start_time, end_time)));
+}
+    //End of cuda function call    
     cudaGraphDependencyCsv.close();
     cudaGraphDependencyCsv.clear();
     
