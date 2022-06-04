54609a8c396392951dd23bb01ecf1e05f2c0d43f
HZjiadong <guojiadonghz@gmail.com>
2022-06-04 20:04:02 +0200

    debug reference errors

--- yesCudaGraphDependance/explicitWithoutDependency/main.cpp
+++ yesCudaGraphDependance/explicitWithoutDependency/main.cpp
@@ -247,7 +247,7 @@ cublasStatus_t cublasDgemm(cublasHandle_t handle,
 */
 //Multiply the arrays A and B on GPU and save the result in C
 //C(m,n) = A(m,k) * B(k,n)
-int gpu_blas_mmul(const double *A, const double *B, double *C, const int m, const int k, const int n, cudaGraph_t* graph){
+int gpu_blas_mmul(const double *A, const double *B, double *C, const int m, const int k, const int n, cudaGraph_t graph){
     int lda=m, ldb=k, ldc=m;
     const double alf = 1.5;
     const double bet = 0.5;
