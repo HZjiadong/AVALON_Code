f1fb796de4cfa4ac80b57d86002a9ac1848ac65e
HZjiadong <guojiadonghz@gmail.com>
2022-06-07 11:20:03 +0200

    debug gpu_blas_mull 2

--- yesCudaGraph/explicitGraphApi/yesDependency/main.cpp
+++ yesCudaGraph/explicitGraphApi/yesDependency/main.cpp
@@ -248,7 +248,7 @@ cublasStatus_t cublasDgemm(cublasHandle_t handle,
 //Multiply the arrays A and B on GPU and save the result in C
 //C(m,n) = A(m,k) * B(k,n)
 int gpu_blas_mmul(const double *A, const double *B, double *C, const int M, const int K, const int N, cudaGraph_t graph){
-    int lda=m, ldb=k, ldc=m;
+    int lda=M, ldb=K, ldc=N;
     const double alf = 1.5;
     const double bet = 0.5;
     const double *alpha = &alf;
